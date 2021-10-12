c496c3efc95da55f02452f7cc66702a373d9f5f9
Jonathan Rivas <jonathan.rivas@student.passerellesnumeriques.org>
2021-10-13 04:55:47 +0800

    update

--- src/app/page-stats/page-stats.component.ts
+++ src/app/page-stats/page-stats.component.ts
@@ -7,11 +7,70 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./page-stats.component.css']
 })
 export class PageStatsComponent implements OnInit {
-  pageId:string;
+  pageId: string;
+  num = 2
+  rows = []
+  number = 15
+  dates = [
+    { submitted: 3, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 4, unfinished: 2, cancelled: 1, visited: 7 },
+    { submitted: 6, unfinished: 0, cancelled: 1, visited: 0 },
+    { submitted: 9, unfinished: 2, cancelled: 1, visited: 7 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 4, unfinished: 2, cancelled: 1, visited: 7 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 1, unfinished: 2, cancelled: 1, visited: 7 },
+    { submitted: 0, unfinished: 0, cancelled: 0, visited: 7 },
+    { submitted: 6, unfinished: 0, cancelled: 1, visited: 0 },
+    { submitted: 1, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 0, unfinished: 0, cancelled: 0, visited: 0 },
+    { submitted: 4, unfinished: 2, cancelled: 1, visited: 7 },
+    { submitted: 0, unfinished: 2, cancelled: 0, visited: 0 },
+    { submitted: 6, unfinished: 0, cancelled: 1, visited: 0 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 4, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 0, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 5, unfinished: 2, cancelled: 1, visited: 0 },
+    { submitted: 9, unfinished: 2, cancelled: 1, visited: 7 },
+  ]
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const nums = new Array(this.number)
+
+
+
+    for (let i = 0; i < nums.length; i++) { this.rows.push(i) }
+    this.showRows()
     this.pageId = this.route.snapshot.paramMap.get('pageId');
+  }
+
+  addRow() {
+    const nums = new Array(this.number += 5)
+    this.rows = []
+    for (let i = 0; i < nums.length; i++) { this.rows.push(i) }
+    this.showRows()
+  }
+
+  minusRow() {
+    const nums = new Array(this.number -= 5)
+    this.rows = []
+    for (let i = 0; i < nums.length; i++) { this.rows.push(i) }
+    this.showRows()
+  }
+
+  showRows() {
+    if (this.rows.length > 20) { this.num = 5 }
+    if (this.rows.length > 40) { this.num = 10 }
+    if (this.rows.length > 100) {
+      let n = this.rows.length
+      while (n > 100) {
+        this.num += 10
+        n -= 100
+      }
+    }
   }
 
 }
